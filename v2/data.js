7869f02b8a6210c561d13b7b34d4a234347f1118
Christof Buehrer <christof@buehrer.me>
2023-06-26 20:33:05 +0200

    add: montfortfest

--- v2/data.js
+++ v2/data.js
@@ -1,18 +1,17 @@
 config = {
-  version: 1,
-  dorffasnet: {
+  "dorffasnet": {
     realname: "Dorffasnet",
     articles: {
-      "bier":        { name: "Bier/Most",    price: 3.50, depo: "flasche" },
-      "weinschorle": { name: "Weinsch.",     price: 4.00, depo: "flasche" },
-      "sekt":        { name: "Sekt",         price: 3.50, depo: "flasche" },
-      "sektflasche": { name: "Sektfla.",     price: 13.00,depo: "flasche" },
-      "saftflasche": { name: "Saftfla.",     price: 5.00, depo: "flasche" },
-      "softdrink":   { name: "Softdrink",    price: 2.50, depo: "flasche" },
-      "longdrink":   { name: "Longdrink",    price: 4.00, depo: "flasche" },
-      "kurzer":      { name: "Kurzer",       price: 1.50, depo: "kein"    },
-      "gluehwein":   { name: "Gl&uuml;hwein",price: 2.50, depo: "flasche" },
-      "kinderpunsch":{ name: "Kinderp.",     price: 2.00, depo: "flasche" }
+      "bier": { name: "Bier/Most", price: 3.50, depo: "flasche" },
+      "weinschorle": { name: "Weinsch.", price: 4.00, depo: "flasche" },
+      "sekt": { name: "Sekt", price: 3.50, depo: "flasche" },
+      "sektflasche": { name: "Sektfla.", price: 13.00, depo: "flasche" },
+      "saftflasche": { name: "Saftfla.", price: 5.00, depo: "flasche" },
+      "softdrink": { name: "Softdrink", price: 2.50, depo: "flasche" },
+      "longdrink": { name: "Longdrink", price: 4.00, depo: "flasche" },
+      "kurzer": { name: "Kurzer", price: 1.50, depo: "kein" },
+      "gluehwein": { name: "Gl&uuml;hwein", price: 2.50, depo: "flasche" },
+      "kinderpunsch": { name: "Kinderp.", price: 2.00, depo: "flasche" }
 
     },
     depo: {
@@ -48,12 +47,12 @@ config = {
   "funken": {
     realname: "Funken",
     articles: {
-      "bier":        { name: "Bier/Most",     price: 3.00, depo: "flasche" },
-      "softdrink":   { name: "Softdrink",     price: 2.50, depo: "flasche" },
-      "kurzer":      { name: "Kurzer",        price: 1.50, depo: "kein"    },
-      "gluehwein":   { name: "Gl&uuml;hwein", price: 2.50, depo: "kein" },
-      "kinderpunsch":{ name: "Kinderp.",      price: 2.00, depo: "kein" },
-      "funkenring":  { name: "Funkenring",    price: 3.00, depo: "kein"    }
+      "bier": { name: "Bier/Most", price: 3.00, depo: "flasche" },
+      "softdrink": { name: "Softdrink", price: 2.50, depo: "flasche" },
+      "kurzer": { name: "Kurzer", price: 1.50, depo: "kein" },
+      "gluehwein": { name: "Gl&uuml;hwein", price: 2.50, depo: "kein" },
+      "kinderpunsch": { name: "Kinderp.", price: 2.00, depo: "kein" },
+      "funkenring": { name: "Funkenring", price: 3.00, depo: "kein" }
     },
     depo: {
       "flasche": { name: "Pfand", price: 1.00 },
@@ -82,15 +81,15 @@ config = {
   "maibaum": {
     realname: "1.Mai",
     articles: {
-      "bier":        { name: "Bier/Most",    price: 3.50, depo: "glas" },
-      "mostp":       { name: "Most Pur",     price: 4.00, depo: "glas" },
-      "weinschorle": { name: "Weinsch.",     price: 4.00, depo: "glas" },
-      "softdrink":   { name: "Softdrink",    price: 2.50, depo: "glas" },
-      "wasser":      { name: "Wasser",       price: 2.00, depo: "glas" },
-      "kaffee":      { name: "Kaffee",       price: 1.50, depo: "kein" },
-      "kuchen":      { name: "Kuchen",       price: 2.00, depo: "kein" },
-      "torte":       { name: "Torte",        price: 2.50, depo: "kein" },
-      "waffel":      { name: "Waffel",       price: 1.00, depo: "kein" },
+      "bier": { name: "Bier/Most", price: 3.50, depo: "glas" },
+      "mostp": { name: "Most Pur", price: 4.00, depo: "glas" },
+      "weinschorle": { name: "Weinsch.", price: 4.00, depo: "glas" },
+      "softdrink": { name: "Softdrink", price: 2.50, depo: "glas" },
+      "wasser": { name: "Wasser", price: 2.00, depo: "glas" },
+      "kaffee": { name: "Kaffee", price: 1.50, depo: "kein" },
+      "kuchen": { name: "Kuchen", price: 2.00, depo: "kein" },
+      "torte": { name: "Torte", price: 2.50, depo: "kein" },
+      "waffel": { name: "Waffel", price: 1.00, depo: "kein" },
     },
     depo: {
       "glas": { name: "Pfand", price: 2.00 },
@@ -112,7 +111,7 @@ config = {
           "mostp",
           "weinschorle",
           "softdrink",
-          "wasser"    
+          "wasser"
         ]
       },
       "bedienung": {
@@ -126,7 +125,33 @@ config = {
           "kaffee",
           "kuchen",
           "torte",
-          "waffel"    
+          "waffel"
+        ]
+      }
+    }
+  },
+  "montfortfest": {
+    "realname": "Montfortfest",
+    articles: {
+      "bier": { name: "Bier/Most", price: 4.00, depo: "glas" },
+      "pils": { name: "Pils", price: 3.50, depo: "flasche" },
+      "cola": { name: "Cola/Fanta", price: 3.00, depo: "flasche" },
+      "spezi": { name: "Spezi", price: 3.00, depo: "flasche" },
+      "wasser": { name: "Wass/Apfels", price: 2.50, depo: "flasche" }
+    },
+    depo: {
+      "glas": { name: "Glas", price: 3.00 },
+      "flasche": { name: "Flasche", price: 1.00 }
+    },
+    "locations": {
+      "verkauf": {
+        realname: "Verkauf",
+        articles: [
+          "bier",
+          "pils",
+          "cola",
+          "spezi",
+          "wasser"
         ]
       }
     }
