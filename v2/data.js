4e5bef8d6fc6855164d370129cd6647b8e55a2cf
cbuehrer <49547208+cbuehrer@users.noreply.github.com>
2024-02-03 14:26:58 +0100

    adjust dorffasnet add weiberball

--- v2/data.js
+++ v2/data.js
@@ -60,10 +60,10 @@ config = {
   "dorffasnet": {
     realname: "Dorffasnet",
     articles: {
-      "bier": { name: "Bier/Most", price: 3.00, depo: "flasche" },
-      "weinschorle": { name: "Weinsch.", price: 4.00, depo: "flasche" },
+      "bier": { name: "Bier/Most", price: 3.50, depo: "flasche" },
+      "weinschorle": { name: "Weinsch.", price: 4.50, depo: "flasche" },
       "sekt": { name: "Sekt", price: 3.50, depo: "flasche" },
-      "sektflasche": { name: "Sektfla.", price: 13.00, depo: "flasche" },
+      "sektflasche": { name: "Sektfla.", price: 14.00, depo: "flasche" },
       "saftflasche": { name: "SAFTfla.", price: 5.00, depo: "flasche" },
       "softdrink": { name: "Softdrink", price: 2.50, depo: "flasche" },
       "longdrink": { name: "Longdrink", price: 4.00, depo: "flasche" },
@@ -257,6 +257,35 @@ config = {
           "bieranti",
           "spezi",
           "wasser"
+        ]
+      }
+    }
+  },
+  "nzttweiberball": {
+    realname: "Weiberball Tettnang",
+    articles: {
+      bier: { name: "Bier/Radler", price: 3.50, depo: "flasche" },
+      alkfrei: { name: "Alkfrei.", price: 2.50, depo: "flasche" },
+      sekt: { name: "Sekt Glas", price: 3.00, depo: "flasche" },
+      sektflasche: { name: "Sekt Flasche", price: 17.00, depo: "flasche" },
+      longdrink: { name: "Longdrink", price: 4.50, depo: "flasche"},
+      weins: { name: "Weinsch.", price: 4.50, depo: "flasche" }
+
+    },
+    depo: {
+      "flasche": { name: "Flasche", price: 2.00 },
+    },
+    locations: {
+      ausschank: {
+        realname: "Ausschank",
+        articles: [
+          "bier",
+          "alkfrei",
+          "sekt",
+          "sektflasche",
+          "longdrink",
+          "weins"
+
         ]
       }
     }
