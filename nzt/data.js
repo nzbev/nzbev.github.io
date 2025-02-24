3d431b30c5262d46f7eab283d6fbc07924bf6484
Christof Buehrer <christof@buehrer.me>
2025-02-24 18:27:01 +0100

    fix: preise 2025

--- nzt/data.js
+++ nzt/data.js
@@ -13,7 +13,7 @@ config = {
     realname: "Weiberball Tettnang",
     articles: {
       bier: { name: "Bier/Radler", price: 3.50, depo: "flasche" },
-      alkfrei: { name: "Alkfrei.", price: 2.50, depo: "flasche" },
+      alkfrei: { name: "Alkfrei.", price: 3.00, depo: "flasche" },
       sekt: { name: "Sekt Glas", price: 3.00, depo: "flasche" },
       sektflasche: { name: "Sekt Flasche", price: 17.00, depo: "flasche" },
       longdrink: { name: "Longdrink", price: 4.50, depo: "flasche"},
