3b1ccd679400e68d8b80020e43d44e7631c14d9d
Christof Buehrer <christof@buehrer.me>
2025-02-24 18:30:03 +0100

    fix: preise 2025

--- nzt/data.js
+++ nzt/data.js
@@ -12,12 +12,12 @@ config = {
   "nzttweiberball": {
     realname: "Weiberball Tettnang",
     articles: {
-      bier: { name: "Bier/Radler", price: 3.50, depo: "flasche" },
-      alkfrei: { name: "Alkfrei.", price: 3.00, depo: "flasche" },
-      sekt: { name: "Sekt Glas", price: 3.00, depo: "flasche" },
-      sektflasche: { name: "Sekt Flasche", price: 17.00, depo: "flasche" },
-      longdrink: { name: "Longdrink", price: 4.50, depo: "flasche"},
-      weins: { name: "Weinsch.", price: 4.50, depo: "flasche" }
+      bier: { name: "Bier/Radler", price: 3.50, depo: "flasche", pname: "Bier/Radler" },
+      alkfrei: { name: "Alkfrei.", price: 3.00, depo: "flasche", pname: "Alkoholfrei" },
+      sekt: { name: "Sekt Glas", price: 3.00, depo: "flasche", pname: "Glas Sekt" },
+      sektflasche: { name: "Sekt Flasche", price: 17.00, depo: "flasche", pname: "Flasche Sekt" },
+      longdrink: { name: "Longdrink", price: 4.50, depo: "flasche", pname: "Longdrink" },
+      weins: { name: "Weinsch.", price: 4.50, depo: "flasche", pname: "Weinschorle" }
 
     },
     depo: {
@@ -33,7 +33,14 @@ config = {
           "sektflasche",
           "longdrink",
           "weins"
-
+        ],
+        pricelist: [
+          "bier",
+          "alkfrei",
+          "sekt",
+          "sektflasche",
+          "longdrink",
+          "weins"
         ]
       }
     }
